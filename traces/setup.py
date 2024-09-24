f001e4c01cb8a4b7dce52805a435e42e30e962ec
pratacosmin <mcosmin1995@gmail.com>
2024-09-24 12:20:31 +0300

    traces as new package

--- traces/setup.py
+++ traces/setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="pythia-traces",  # This is the package name on PyPI
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(where="."),
+    package_dir={"traces": "traces"},
     install_requires=["openlit==1.22.0"],
     python_requires=">=3.10",
     description="The Pythia Traces package for event tracing",
