dff12342abea63231b987d3c97c08e96cbf46c21
pratacosmin <mcosmin1995@gmail.com>
2024-09-25 14:40:39 +0300

    update setup for trace class

--- traces/setup.py
+++ traces/setup.py
@@ -3,7 +3,8 @@ import setuptools
 setuptools.setup(
     name="pythia-traces",  # This is the package name on PyPI
     version="0.1.0",
-    packages=["traces"],
+    package_dir={"pythia": "traces"},
+    packages=setuptools.find_packages(where="."),
     install_requires=["openlit==1.22.0"],
     python_requires=">=3.10",
     description="The Pythia Traces package for event tracing",
